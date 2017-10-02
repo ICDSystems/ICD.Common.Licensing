152f000046f609176cec2146b52895be5c374850
Chris Cameron <chris.cameron@wedoresi.com>
2017-10-02 15:58:29 -0400

    Initial commit

---
+++ ICD.Common.Licensing/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Resources;
+
+[assembly: AssemblyTitle("ICD.Common.Licensing")]
+[assembly: AssemblyCompany("ICD Systems")]
+[assembly: AssemblyProduct("ICD.Common.Licensing")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyVersion("1.0.0.*")]
