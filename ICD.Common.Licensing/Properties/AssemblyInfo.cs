15383037f19eb70c3f4637849eda6f6921922ddd
Chris Cameron <chris.cameron@wedoresi.com>
2018-09-24 11:47:36 -0400

    chore: Updating assembly info

--- ICD.Common.Licensing/Properties/AssemblyInfo.cs
+++ ICD.Common.Licensing/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Licensing")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Licensing")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("3.1.0.0")]
