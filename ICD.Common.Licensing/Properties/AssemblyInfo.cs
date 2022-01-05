5de1104b06ed00b99de3e3b5cd35be680b8df17b
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 16:27:17 -0500

    chore: Updating AssemblyInfo year

--- ICD.Common.Licensing/Properties/AssemblyInfo.cs
+++ ICD.Common.Licensing/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Common.Licensing")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Licensing")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("4.0.0.0")]
