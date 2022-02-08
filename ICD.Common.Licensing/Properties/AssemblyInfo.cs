af3233a81a1d636913324a07c421c9ec78b68962
Drew Tingen <drew.tingen@icdsystems.com>
2022-02-08 15:35:21 -0500

    chore: Update Changelog, increment assembly patch version

--- ICD.Common.Licensing/Properties/AssemblyInfo.cs
+++ ICD.Common.Licensing/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Common.Licensing")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
-[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyVersion("3.1.2.0")]
