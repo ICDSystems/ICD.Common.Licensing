eb6f2e213e17f69648071331afc94e96f0811893
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-21 16:44:34 -0400

    chore: Remove Obfuscation

--- ICD.Common.Licensing/ObfuscationSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: Obfuscation(Feature = "platform api: System.Threading.Thread, System.Reflection.*, System.IO.Stream, System.Windows.Forms.*", Exclude = true)]
-[assembly: Obfuscation(Feature = "rename symbol names with printable characters", Exclude = false)]
-[assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
-[assembly: Obfuscation(Feature = "Apply to type * when class: renaming", Exclude = true, ApplyToMembers = false)]
